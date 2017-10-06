a7d58c3e1e32c340379f3ab17517667839edf220
Franck Verrot <franck@verrot.fr>
2017-10-06 09:10:37 -0700

    Hide Ruby native code representation

--- lib/prelude.rb
+++ lib/prelude.rb
@@ -1,34 +1,32 @@
 module Prelude
   def apply(program)
     program + [
-      Add,
-      Sub,
-      Mul,
-      Div
+      BinOp["add", ->(x, y) { x + y } ],
+      BinOp["sub", ->(x, y) { x - y } ],
+      BinOp["mul", ->(x, y) { x * y } ],
+      BinOp["div", ->(x, y) { x / y } ]
     ]
   end
   module_function :apply
 
   BinOp = ->(name, ruby_func) do
     id_x, id_y = Identifier.new("x"), Identifier.new("y")
+    int_type = Domain.new(NumberType)
     FunDef.new(
       Identifier.new(name),
       FunCallArgs.new([id_x, id_y]),
-      ->(programs, current_function) do
-        program, env = programs
-        finder = ->(id) { [program,env].flatten.find { |a| a.identifier == id } }
-        fun_x, fun_y = finder[id_x], finder[id_y]
-        x = run_eval(fun_x, programs)
-        y = run_eval(fun_y, programs)
-        ruby_func[x, y]
-      end).tap do |fd|
-        int = Domain.new(NumberType)
-        fd.domains = Domains.new([int, int, int])
-      end
+      NativeRubyCode.new(
+        name,
+        ->(programs, current_function) do
+          program, env = programs
+          finder = ->(id) { [program,env].flatten.find { |a| a.identifier == id } }
+          fun_x, fun_y = finder[id_x], finder[id_y]
+          x = run_eval(fun_x, programs)
+          y = run_eval(fun_y, programs)
+          ruby_func[x, y]
+        end,
+        Domains.new([int_type, int_type, int_type])
+      ),
+      Domains.new([int_type, int_type, int_type]))
   end
-
-  Add = BinOp["add", ->(x, y) { x + y } ]
-  Sub = BinOp["sub", ->(x, y) { x - y } ]
-  Mul = BinOp["mul", ->(x, y) { x * y } ]
-  Div = BinOp["div", ->(x, y) { x / y } ]
 end
